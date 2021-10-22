3ef17e2f199ccc3d55b134aad2010c852a33d46e
qvdp <vanderperre.quentin@gmail.com>
2021-10-22 14:29:27 +0200

    v-for

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,14 +1,13 @@
 
 <template>
-<div class="header bg-black">
+<div class="header bg-black bg-right bg-contain bg-no-repeat bg-hero">
 <div class="w-full flex justify-between  p-1 px-7 position-static">
 <div class="flex p-1 m-3 flex-row justify-center">
-<img class="size-x-1" src="../assets/images/Logo-MI-1.png" alt="">
+<logo />
 <ul class="text-white block p-4 pl-10">
-<NuxtLink to="/">Nos valeurs</NuxtLink>
-<NuxtLink to="/">Notre solutions</NuxtLink>
-<NuxtLink to="/">Nos partenaire</NuxtLink>
-<NuxtLink to="/">L'équipe</NuxtLink>
+<NuxtLink v-for="{ link, title } in links" :key="link" :to="link">
+  {{ title }}
+</NuxtLink>
 </ul>
 </div>
 <div class="button pt-4">
@@ -17,29 +16,48 @@
 </div>
 <div class="main grid p-10" alig>
 <img class="mb-5" src="../assets/images/Logo-MI-1.png" alt="">
-<div class="content">
+<div id="solutions" class="content">
 <h1 class="text-white text-5xl">Des solution adaptées</h1>
 <h2 class="text-red-800 text-4xl">à vos besions</h2>
 <p class="text-white text-2xl">Amin aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem</p>
 </div>
 </div>
-<div class="nos-valeurs flex grid-cols-3 justify-around text-white border-t-2 pt-5 pb-5 ">
-<div class="valeur-1 text-white border rounded pb-2">
-<img  class="" src="../assets/images/logo-cic.jpeg" alt="">
-<h3 class=" text-center">Valeur 1</h3>
-<p class="text-center">Benoit, faut rédiger un potit text slp</p>
-</div>
-<div class="valeur-2 text-white border rounded">
-<img  class="" src="../assets/images/logo-cic.jpeg" alt="">
-<h3 class="text-center">Valeur 1</h3>
-<p class="text-center">Benoit, faut rédiger un potit text slp</p>
-</div>
-<div class="valeur-3 text-white border rounded">
-<img  class="" src="../assets/images/logo-cic.jpeg" alt="">
-<h3 class="text-center">Valeur 1</h3>
-<p class="text-center">Benoit, faut rédiger un potit text slp</p>
-</div>
+<div class="nos-valeurs grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 sm:text-white md:text-blue-900 justify-around border-t-2 pt-5 pb-5 ">
+  <div class="valeur-1 border rounded pb-2">
+    <img  class="" src="../assets/images/logo-cic.jpeg" alt="">
+   <h3 class=" text-center">Valeur 1</h3>
+   <p class="text-center">Benoit, faut rédiger un potit text slp</p>
+  </div>
+  <div class="valeur-2 border rounded">
+    <img  class="" src="../assets/images/logo-cic.jpeg" alt="">
+    <h3 class="text-center">Valeur 1</h3>
+    <p class="text-center">Benoit, faut rédiger un potit text slp</p>
+  </div>
+  <div id="values" class="valeur-3 border rounded">
+    <img  class="" src="../assets/images/logo-cic.jpeg" alt="">
+    <h3 class="text-center">Valeur 1</h3>
+    <p class="text-center">Benoit, faut rédiger un potit text slp</p>
+  </div>
 </div>
 </div>
 </template>
-<script> export default {}</script>
+<script>
+import logo from '@/components/Logo'
+
+export default {
+  name: 'Hero',
+  components: {
+    logo
+  },
+  data () {
+    return {
+      links: [
+        { link: '/#values', title: 'Nos valeurs' },
+        { link: '/#solutions', title: 'Notre solutions' },
+        { link: '/', title: 'Nos partenaire' },
+        { link: '/', title: 'L\'équipe' },
+        { link: '/', title: 'bloc' }
+      ]
+    }
+  }
+}</script>
